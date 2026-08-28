54c894bdc138c4bff0c6b8ac9139277a47d69314
AchirayanSaenkayan <mm79mm79oo@gmail.com>
2026-08-28 14:43:41 +0700

    Finish SetBall

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -19,12 +19,19 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     private int point;
     [SerializeField]
     private BallColor color;
+    [SerializeField]
+    private MeshRenderer rd;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
         GameManager.instance.PlayerScore += point;
         Destroy(gameObject);
+    }
+
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
     }
 
     private void Start()
@@ -35,5 +42,44 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     private void Update()
     {
         
+    }
+    public void SetColorAndPoint(BallColor color)
+    {
+        this.color = color;
+        switch (color)
+        {
+            case BallColor.White:
+                rd.material.color = Color.white;
+                point = 0;
+                break;
+            case BallColor.Red:
+                rd.material.color = Color.red;
+                point = 1;
+                break;
+            case BallColor.Yellow:
+                rd.material.color = Color.yellow;
+                point = 2;
+                break;
+            case BallColor.Green:
+                rd.material.color = Color.green;
+                point = 3;
+                break;
+            case BallColor.Brown:
+                rd.material.color = new Color(0.6f, 0.3f, 0.1f);
+                point = 4;
+                break;
+            case BallColor.Blue:
+                rd.material.color = Color.blue;
+                point = 5;
+                break;
+            case BallColor.Pink:
+                rd.material.color = new Color(1f, 0.4f, 0.7f);
+                point = 6;
+                break;
+            case BallColor.Black:
+                rd.material.color = Color.black;
+                point = 7;
+                break;
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,13 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private int playerScore;
-        public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
+    public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
+
+        [SerializeField]
+        private GameObject[] ballPositions;
+
+        [SerializeField]
+        private GameObject ballPrefab;
 
     public static GameManager instance;
 
@@ -15,6 +21,26 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        SetBall(BallColor.White, 0);
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
+    }
 
+    void Update()
+    {
+
+    }
+
+    private void SetBall(BallColor col, int i)
+    {
+    GameObject obj = Instantiate(ballPrefab, ballPositions[i].transform.position, Quaternion.identity);
+    Ball b = obj.GetComponent<Ball>();
+    b.SetColorAndPoint(col);
+}
     }
 }
