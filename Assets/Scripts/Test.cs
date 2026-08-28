1de7a72ef99859f40da6f7e15cf8eaa8d78ddaae
AchirayanSaenkayan <mm79mm79oo@gmail.com>
2026-08-28 11:36:33 +0700

    Test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
